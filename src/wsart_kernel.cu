55a6004d076b34c255a2065eece3ec9cd5c652da
Omer Cinar <omercinar253@gmail.com>
2023-11-09 15:15:37 -0500

    Minor changes and initial function sig

---
+++ src/wsart_kernel.cu
@@ -0,0 +1,14 @@
+/**
+ * @file wsart_kernel.cu
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-11-09
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+__global__ void wsart() {
+
+} //wsart
