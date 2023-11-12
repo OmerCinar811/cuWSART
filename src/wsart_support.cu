ecdd8c42827b80096d8766d62690e4c3cab74d0c
Omer Cinar <omercinar253@gmail.com>
2023-11-11 23:24:49 -0500

    Added initial stages of read_png, although it is not yet finished

--- src/wsart_support.cu
+++ src/wsart_support.cu
@@ -24,3 +24,58 @@
 //     fclose(fp);
 // }
 
+/**
+ * @brief Reads in a png
+ *        Assumes that file is a png
+ * 
+ * @param file_name string for file name
+ */
+void read_png(char *file_name) {
+
+    FILE *fp = fopen(file_name, "rb"); // open file
+
+    if(!fp) { //check if file was able to be opened
+        printf("Could not open file\n");
+        exit(FILE_ERR);
+    } 
+
+    // Creating png read struct and checking if it was created
+    png_structp p_png = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+    
+    if(!p_png) {
+        printf("Could not create png read struct pointer\n");
+        exit(PNG_STRUCT_ERR);
+    }
+
+    // Creating png info structure and checking if it was created
+    png_infop p_info = png_create_info_struct(p_png);
+
+    if(!p_info) {
+        png_destroy_read_struct(&p_png, (png_infopp)NULL, (png_infopp)NULL); // free any mem of png structs
+        printf("Could not create png info struct pointer\n");
+        exit(PNG_INF_STR_ERR);
+    } 
+
+    // set file pointer to png struct
+    png_init_io(p_png, fp);
+    png_set_sig_bytes(p_png, 8); //tells library to ignore first 8 bytes (aka magic number)
+
+
+    // read info from the png, including the info before the data and the header
+    png_read_info(p_png, p_info);
+    uint32_t width, height;
+    int bit_depth, color_type, interlace_type, compression_type, filter_method;
+    
+    // Get image header data
+    png_get_IHDR(p_png, p_info,  &width, &height, &bit_depth, &color_type, &interlace_type, NULL, NULL);
+
+
+    // Read the actual png image data
+    png_bytepp row_pointers = (png_bytepp)png_malloc(p_png, sizeof(png_bytepp)*height);
+    for(int i = 0; i < height; i++) {
+        row_pointers[i] = (png_bytep)png_malloc(p_png, width * 1);
+    }
+
+     
+
+} // read_png
--- src/wsart_support.h
+++ src/wsart_support.h
@@ -2,8 +2,17 @@
 #define _WSART_SUPPORT_H
 
 #include "png.h"
+#include "stdint.h"
 
-void read_png(char*);
-void write_png(char*);
+
+#define FILE_ERR        1
+#define PNG_STRUCT_ERR  2
+#define PNG_INF_STR_ERR 3
+
+
+// void read_png(char*);
+// void write_png(char*);
+
+
 
 #endif
